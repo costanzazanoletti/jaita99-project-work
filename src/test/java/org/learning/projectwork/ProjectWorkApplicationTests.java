8e8a8018c6cba2a988ccc51377271b51c7ef568f
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2024-01-12 11:08:47 +0100

    setup progetto Spring

---
+++ src/test/java/org/learning/projectwork/ProjectWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.projectwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
