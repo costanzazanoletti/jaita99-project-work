3c8c291fc338a347fa7e19f72505fa44171cf401
ghostwriter <ghost@writer.com>
2024-01-12 11:27:32 +0100

    Aggiungo ContactController, aggiungo attributi a IndexController

--- src/main/java/org/learning/projectwork/controller/IndexController.java
+++ src/main/java/org/learning/projectwork/controller/IndexController.java
@@ -1,13 +1,16 @@
 package org.learning.projectwork.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
   @GetMapping
-  public String home(){
+  public String home(Model model){
+    model.addAttribute("title", "Welcome");
+    model.addAttribute("user", "Ghost");
     return "home";
   }
 }
